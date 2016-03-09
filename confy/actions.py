14200fc261a61e5fdd293b04f39242945c08e1fe
calebh93 <hawkins.caleb93@gmail.com>
2016-03-09 13:07:42 -0800

    Confy post submission

--- confy/actions.py
+++ confy/actions.py
@@ -3,7 +3,13 @@ import json as _json
 
 
 def get_page_content(id):
-    print _json.loads(_api.rest(id + "?expand=body.storage"))["body"]["storage"]["value"]
+    """Return XHTML content of a page.
+
+    Parameters:
+    - id: id of a Confluence page.
+    """
+    data = _json.loads(_api.rest(id + "?expand=body.storage"))
+    return data["body"]["storage"]["value"]
 
 
 if __name__ == "__main__":
