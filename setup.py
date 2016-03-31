f0171b960603c6f74c36ac3575a5193c2389e7a2
calebh93 <hawkins.caleb93@gmail.com>
2016-03-31 15:16:30 -0700

    Properly updated setup.py. NOW it's official!

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name = 'confy',
     packages = ['confy'],
-    version = '0.1.1',
+    version = '0.2',
     description = 'An API wrapper for Atlassian Confluence',
     author = 'Caleb Hawkins',
     author_email = 'hawkins.caleb93@gmail.com',
     url = 'https://github.com/muddytm/confy',
-    download_url = 'https://github.com/muddytm/confy/tarball/0.1.1',
+    download_url = 'https://github.com/muddytm/confy/tarball/0.2',
     keywords = ['confluence'],
     classifiers = [],
 )
