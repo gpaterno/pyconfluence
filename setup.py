b859eabffcd5f8194e800e873ca4e7a78fb79422
calebh93 <hawkins.caleb93@gmail.com>
2016-04-07 13:42:19 -0700

    Changing some stuff in setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name='pyconfluence',
     packages=['pyconfluence'],
-    version='1.0',
+    version='1.1',
     description='An API wrapper for Atlassian Confluence',
     author='Caleb Hawkins',
     author_email='hawkins.caleb93@gmail.com',
-    url='https://github.com/muddytm/pyconfluence',
-    download_url='https://github.com/muddytm/pyconfluence/tarball/1.0',
+    url='https://github.com/FulcrumIT/pyconfluence',
+    download_url='https://github.com/FulcrumIT/pyconfluence/tarball/1.1',
     keywords=['confluence'],
     classifiers=[],
 )
