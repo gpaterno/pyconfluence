8cd121d1540a4a720fc53b218328843a8cadeb47
calebh93 <hawkins.caleb93@gmail.com>
2016-04-20 09:22:59 -0700

    Changes in prep for release V1.1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name='pyconfluence',
     packages=['pyconfluence'],
-    version='1.1',
+    version='1.1.1',
     description='An API wrapper for Atlassian Confluence',
     author='Caleb Hawkins',
     author_email='hawkins.caleb93@gmail.com',
     url='https://github.com/FulcrumIT/pyconfluence',
-    download_url='https://github.com/FulcrumIT/pyconfluence/tarball/1.1',
+    download_url='https://github.com/FulcrumIT/pyconfluence/tarball/1.1.1',
     keywords=['confluence'],
     classifiers=[],
 )
